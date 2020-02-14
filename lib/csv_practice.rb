49e2a7a547c6b95c0baeee5db6b6d90b3c48416b
Yaz <yasmineoshaugh@gmail.com>
2020-02-14 11:29:20 -0800

    added to get_all_olympic_athletes: return CSV.read(..).map(&:to_h). This passes some but not all tests

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -2,7 +2,10 @@ require 'csv'
 require 'awesome_print'
 
 def get_all_olympic_athletes(filename)
-
+  return CSV.read(filename, headers: true).map(&:to_h)
+  #  do |medalist|
+  #   medalist.each do |info|
+  #   end
 end
 
 def total_medals_per_team(olympic_data)
@@ -11,3 +14,5 @@ end
 
 def get_all_gold_medalists(olympic_data)
 end
+
+#get_all_olympic_athletes("data/athlete_events.csv")
