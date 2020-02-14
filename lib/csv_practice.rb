93898c539aad7a52fbebbd99f0240ee19fc34252
Yaz <yasmineoshaugh@gmail.com>
2020-02-14 12:20:44 -0800

    get_olympic_athletes passes all tests

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -2,10 +2,30 @@ require 'csv'
 require 'awesome_print'
 
 def get_all_olympic_athletes(filename)
-  return CSV.read(filename, headers: true).map(&:to_h)
-  #  do |medalist|
-  #   medalist.each do |info|
-  #   end
+  all_medalists = []
+  CSV.read(filename, headers: true).each do |medalist|
+    # medalist is an instance of CSV::Row
+    medalist = medalist.to_hash
+    medalist.delete("Sex")
+    medalist.delete("Age")
+    medalist.delete("Weight")
+    medalist.delete("NOC")
+    medalist.delete("Games")
+    medalist.delete("Season")
+    all_medalists <<  medalist
+    # { # keys need to be strings in that case we use => to assign value
+    #   ID: medalist["ID"],
+    #   Name: medalist["Name"],
+    #   Height: medalist["Height"],
+    #   Team: medalist["Team"],
+    #   Year: medalist["Year"],
+    #   City: medalist["City"],
+    #   Sport: medalist["Sport"],
+    #   Event: medalist["Event"],
+    #   Medal: medalist["Medal"]
+    # }
+  end
+  return all_medalists
 end
 
 def total_medals_per_team(olympic_data)
@@ -14,5 +34,3 @@ end
 
 def get_all_gold_medalists(olympic_data)
 end
-
-#get_all_olympic_athletes("data/athlete_events.csv")
